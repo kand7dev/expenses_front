32db0142d30cf5795b209a91e15025c977e7cb6c
kand7dev <kand7dev@gmail.com>
2024-03-09 02:18:48 +0200

    implement setExpenses action-reducer

---
+++ src/services/expenses.js
@@ -0,0 +1,28 @@
+import { ActionCreators } from "../app/expensesReducer";
+
+// Responsible for making HTTP requests.
+// HTTP Methods will happen here.
+
+export const GetExpenses = async (dispatch) => {
+  try {
+    // API call happens here.
+    // Pollting with hard-coded data for now.
+    // dispatch is used to trigger a state change. expenses[] will be stored in our state afte dispatch.
+    const expenses = [
+      {
+        id: 1,
+        description: "Groceries",
+        ammount: 23.16,
+        id: 2,
+        description: "Gas",
+        ammount: 18.52,
+        id: 3,
+        description: "Student Loans",
+        ammount: 600,
+      },
+    ];
+    dispatch(ActionCreators.setExpenses(expenses));
+  } catch {
+    console.log("Error");
+  }
+};
