15163cb24f7a80da6780a78c4e7bc413c1160c71
kand7dev <kand7dev@gmail.com>
2024-03-09 02:55:09 +0200

    activate reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import expensesReducer from "./expensesReducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    expensesReducer: expensesReducer,
+  },
 });
