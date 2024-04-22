0aeb4c9adc1d40ccaf2e8128bb2ef3635118b1f0
kand7dev <kand7dev@gmail.com>
2024-04-23 01:24:27 +0300

    add authenticationSlice to our store reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import expensesSlice from "./expensesSlice";
 import ToastMiddleware from "../middlewares/ToastMiddleware";
+import  authenticationSlice  from "./authSlice";
 
 export const store = configureStore({
   reducer: {
     expensesSlice: expensesSlice,
+    authenticationSlice: authenticationSlice
   },
   middleware: [ToastMiddleware]
 });
