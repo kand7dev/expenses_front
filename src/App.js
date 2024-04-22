84b65cd3f5a312909eabddb5a3f014ba8e973cba
kand7dev <kand7dev@gmail.com>
2024-04-23 01:24:08 +0300

    render the SignInPage component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
 import { ToastContainer } from "react-toastify";
+import SignInPage from "./components/SignInPage";
+
 
 const App = () => (
-  <div style={{ width: "60%", margin: "auto" }}>
-    <ToastContainer/>
-    <h3>New Expense</h3>
-    <ExpenseForm />
-    <hr style={{ border: "1px solid grey" }} />
-    <h3>Your Expenses</h3>
-    <ExpenseList />
-  </div>
+  <SignInPage></SignInPage>
+  // <div style={{ width: "60%", margin: "auto" }}>
+  //   <ToastContainer/>
+  //   <h3>New Expense</h3>
+  //   <ExpenseForm />
+  //   <hr style={{ border: "1px solid grey" }} />
+  //   <h3>Your Expenses</h3>
+  //   <ExpenseList />
+  // </div>
 );
 
 export default App;
