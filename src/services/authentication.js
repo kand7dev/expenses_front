3bc2096ecdefac0179984039dbc1a70bc71907ac
kand7dev <kand7dev@gmail.com>
2024-04-23 01:23:08 +0300

    use axios to hit back end api end points for log in and sign up

---
+++ src/services/authentication.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { userAuthenticated } from "../app/authSlice";
+
+const axiosInstance = axios.create({
+    baseURL: `${process.env.REACT_APP_BASE_URL}/Authentication`,
+});
+
+export const SignUp = async (dispatch, credentials) => {
+    try {
+        const { data } = await axiosInstance.post("/signup", credentials);
+        dispatch(userAuthenticated(data));
+    } catch {
+        console.log("Error!");
+    }
+};
+
+export const SignIn = async (dispatch, credentials) => {
+    try {
+        const { data } = await axiosInstance.post("/signin", credentials)
+        dispatch(userAuthenticated(data));
+    } catch {
+        console.log("Error!");
+    }
+}
