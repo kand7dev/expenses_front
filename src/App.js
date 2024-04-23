0632dce0537c966fd734144d55cc71780cf37968
kand7dev <kand7dev@gmail.com>
2024-04-23 18:52:32 +0300

    use react-router-dom for redirections

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import ExpenseList from "./components/ExpenseList";
-import ExpenseForm from "./components/ExpenseForm";
-import { ToastContainer } from "react-toastify";
 import SignInPage from "./components/SignInPage";
+import SignUpPage from "./components/SignUpPage";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import HomePage from "./components/HomePage";
+import { useEffect } from "react";
+import { userAuthenticated } from "./app/authSlice";
+import Navbar from "./components/NavBar"
 
 
-const App = () => (
-  <SignInPage></SignInPage>
-  // <div style={{ width: "60%", margin: "auto" }}>
-  //   <ToastContainer/>
-  //   <h3>New Expense</h3>
-  //   <ExpenseForm />
-  //   <hr style={{ border: "1px solid grey" }} />
-  //   <h3>Your Expenses</h3>
-  //   <ExpenseList />
-  // </div>
-);
+const App = () => {
+  const { isLoggedIn } = useSelector(state => state.authenticationSlice)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("token")
+    if (token !== undefined && token !== null) {
+      dispatch(userAuthenticated({ token }))
+    }
+  }, []);  //eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <HomePage /> : <Navigate replace to="/signin" />} />
+        <Route path="/signup" element={isLoggedIn ? <Navigate replace to="/" /> : <SignUpPage />} />
+        <Route path="/signin" element={isLoggedIn ? <Navigate repalce to="/" /> : <SignInPage />} />
+        <Route path="*" element={<h2>Page not Found</h2>} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
 
 export default App;
