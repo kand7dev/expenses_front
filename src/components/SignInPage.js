7086f469ad953988b84c5d269d53c9eae2da7863
kand7dev <kand7dev@gmail.com>
2024-04-25 00:33:14 +0300

    use toastcontainer in SignIn page

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -3,6 +3,7 @@ import { FormControl, InputGroup, Form, Button } from "react-bootstrap"
 import { SignIn } from "../services/authentication";
 import { useDispatch } from "react-redux";
 import ThirdPartySignIns from "./ThirdPartySignIns"
+import { ToastContainer } from "react-toastify";
 
 
 const SignInPage = () => {
@@ -10,22 +11,24 @@ const SignInPage = () => {
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
 
-    return <div style={{ width: "30rem", margin: "auto", paddingTop: "8px" }}>
-        <Form onSubmit={event => {
-            event.preventDefault();
-            SignIn(dispatch, { username, password })
-        }}>
-            <h4 style={{ textAlign: "center" }}>Welcome back!</h4>
-            <InputGroup className="mb-3" style={{ marginTop: "1rem" }}>
-                <FormControl placeholder="Username" onChange={event => setUsername(event.target.value)}></FormControl>
-            </InputGroup>
-            <InputGroup className="mb-3">
-                <FormControl placeholder="Password" type="password" onChange={event => setPassword(event.target.value)}></FormControl>
-            </InputGroup>
-            <Button type="submit" variant="primary" style={{ margin: "auto", display: "block", width: "10rem" }}>Sign In</Button>
-        </Form>
-        <ThirdPartySignIns />
-    </div >
+    return (
+        <div style={{ width: "30rem", margin: "auto", paddingTop: "8px" }}>
+            <ToastContainer />
+            <Form onSubmit={event => {
+                event.preventDefault();
+                SignIn(dispatch, { username, password })
+            }}>
+                <h4 style={{ textAlign: "center" }}>Welcome back!</h4>
+                <InputGroup className="mb-3" style={{ marginTop: "1rem" }}>
+                    <FormControl placeholder="Username" onChange={event => setUsername(event.target.value)}></FormControl>
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <FormControl placeholder="Password" type="password" onChange={event => setPassword(event.target.value)}></FormControl>
+                </InputGroup>
+                <Button type="submit" variant="primary" style={{ margin: "auto", display: "block", width: "10rem" }}>Sign In</Button>
+            </Form>
+            <ThirdPartySignIns />
+        </div >)
 };
 
 export default SignInPage;
